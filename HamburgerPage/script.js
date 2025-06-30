d35401f013d9634737f20619342e4412a915c390
Veerhan-glitch <83816165+Veerhan-glitch@users.noreply.github.com>
2025-06-30 16:40:20 +0530

    crew

--- HamburgerPage/script.js
+++ HamburgerPage/script.js
@@ -15,4 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
   setupImageHover('.getintouch', 'Images/touch.png', 'Images/touch_s.png');
   setupImageHover('.contactHamburger', 'Images/contacts.png', 'Images/contacts_s.png');
   setupImageHover('.scheduleHamburger', 'Images/schedule.png', 'Images/schedule_s.png');
+  setupImageHover('.crew', 'Images/crew.png', 'Images/crew_s.png');
 });
