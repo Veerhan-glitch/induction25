43d04ab3b487b16d4fba58be7f1235ddbfa5a7fd
Veerhan-glitch <83816165+Veerhan-glitch@users.noreply.github.com>
2025-07-11 22:30:20 +0530

    guest speakers

--- HamburgerPage/script.js
+++ HamburgerPage/script.js
@@ -15,5 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
   setupImageHover('.getintouch', 'Images/touch.png', 'Images/touch_s.png');
   setupImageHover('.contactHamburger', 'Images/contacts.png', 'Images/contacts_s.png');
   setupImageHover('.scheduleHamburger', 'Images/schedule.png', 'Images/schedule_s.png');
+  setupImageHover('.guest', 'Images/guest.png', 'Images/guest_s.png');
   setupImageHover('.crew', 'Images/crew.png', 'Images/crew_s.png');
 });
